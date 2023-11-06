9016292f009a21f3f115334826400b23887b9a0f
c00cjz00 <summerhill001@gmail.com>
2023-11-06 11:08:21 +0800

    Update model.py

--- model.py
+++ model.py
@@ -81,7 +81,7 @@ def load_llm():
 def qa_bot():
 #    embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2",
 #                                       model_kwargs={'device': 'cpu'})
-    embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+    embeddings = HuggingFaceEmbeddings(model_name='/work/u00cjz00/nvidia/all-MiniLM-L6-v2')
     db = FAISS.load_local(DB_FAISS_PATH, embeddings)
     llm = load_llm()
     qa_prompt = set_custom_prompt()
